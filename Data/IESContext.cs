83ba7d909a39edd54be63a9a994427ec22d13866
gabrielladsc <gabriellacorreia@gec.inatel.br>
2021-01-14 18:33:39 -0300

    Criação de layout com bootstrap e jquery

---
+++ Data/IESContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capitulo01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capitulo01.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options){}
+
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        public class IESDbInitializer
+        {
+            public static void Initialize(IESContext context)
+            {
+                context.Database.EnsureCreated();
+
+                if (context.Departamentos.Any())
+                {
+                    return;
+                }
+
+                var departamentos = new Departamento[]
+                {
+                    new Departamento { Nome = "Ciência de Computação" },
+                    new Departamento { Nome = "Ciência de Alimentos" }
+                };
+
+                foreach (Departamento d in departamentos)
+                {
+                    context.Departamentos.Add(d);
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
